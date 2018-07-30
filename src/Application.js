a53602c3d597dfdde1378789f592f0c5a4d47d39
Ahsan Naveed <ahsannaveed007@yahoo.com>
2018-07-29 20:54:10 -0700

    Upload an image ot S3 via Storage API

--- src/Application.js
+++ src/Application.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import { Storage } from 'aws-amplify';
+
 import './Application.css';
+
+class S3Image extends Component {
+  state = { src: null }
+  
+  async componentDidMount() {
+    const { s3Key } = this.props;
+    const src = await Storage.get(s3Key);
+    this.setState(() => ({ src }));
+  }
+  
+  render() {
+    const { src } = this.state;
+    if(!src) return null; 
+    
+    const { alt } = this.props;
+    return (
+      <article>
+        <img src={src} alt={alt} />
+      </article>
+    );
+  }
+}
 
 class Application extends Component {
   state = {
     files: []
   };
+
+  async componentDidMount() {
+    // list all contents of bucket
+    const files = await Storage.list('');
+
+    this.setState(() => ({ files }));
+  }
 
   handleSubmit = event => {
     event.preventDefault();
@@ -12,7 +43,9 @@ class Application extends Component {
     const file = this.fileInput.files[0];
     const { name } = file;
 
-    console.log(file, name);
+    Storage.put(name, file).then((response) => {
+      this.setState(() => ({files: [...this.state.files, response] }));
+    }).catch(console.error);
   };
 
   render() {
@@ -26,7 +59,15 @@ class Application extends Component {
           <input className="full-width" type="submit" />
         </form>
         <section className="Application-images">
-
+          {this.state.files.map(file => {
+            return (
+              <S3Image
+                s3Key={file.key}
+                key={file.key}
+                alt={`graphic-${file.key}`}
+              />
+            );
+          })}
         </section>
       </div>
     );
