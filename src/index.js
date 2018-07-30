05b878907dc16055b98686e959000e841dba791f
Ahsan Naveed <ahsannaveed007@yahoo.com>
2018-07-29 19:45:49 -0700

    Inform react-frontend of aws-backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import Application from './Application';
+import configuration from './aws-exports';
+
+Amplify.configure(configuration);
 
 ReactDOM.render(<Application />, document.getElementById('root'));
